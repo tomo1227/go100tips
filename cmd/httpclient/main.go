becfb6c94938dda0655ad39d64c4fea614ff2ea6
tomo1227 <tomo.soccer1227@gmail.com>
2025-06-21 09:32:45 +0000

    httpclientを修正

--- cmd/httpclient/main.go
+++ cmd/httpclient/main.go
@@ -5,24 +5,18 @@ import (
 )
 
 func main() {
-	// url := "http://localhost:8080/hello"
+	url := "http://localhost:8080/hello"
 
-	// headers := map[string]string{
-	// 	"Content-Type": "application/json",
-	// }
+	headers := map[string]string{
+		"Content-Type": "application/json",
+	}
 
-	// // jsonData := []byte(`{"name":"ChatGPT","language":"Go"}`)
+	resp, err := DoRequest(MethodGet, url, headers, nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	// resp, err := DoRequest(MethodGet, url, headers, nil)
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-
-	// fmt.Println("Status Code:", resp.StatusCode())
-	// fmt.Println("Response Body:", resp.String())
-	method := MethodPost
-
-	fmt.Println("HTTP Method:", method)         // 自動で String() が呼ばれる
-	fmt.Println("Is GET?", method == MethodGet) // false
+	fmt.Println("Status Code:", resp.StatusCode())
+	fmt.Println("Response Body:", resp.String())
 }
